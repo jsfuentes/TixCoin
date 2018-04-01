536835a51c5b29481d3bd913680fccbd8f551a53
Jorge Fuentes <jsjfuentesj@yahoo.com>
2018-03-31 17:13:13 -0700

    Add dynamic event listings

--- routes/events.js
+++ routes/events.js
@@ -11,20 +11,24 @@ var upload = multer({ dest: 'public/uploads/'});
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  // Event.find({}, function (err, events) {
-  //   if (err) return res.send(err);
-  //   // 'athletes' contains the list of athletes that match the criteria.
-  //   console.log("ALL EVENTS: ", events);
-  //   res.render('listing', {events: events});
-  // });
-  console.log("/events");
-  res.render('listings');
+  Event.find({}, function (err, events) {
+    if (err) return res.send(err);
+    // 'athletes' contains the list of athletes that match the criteria.
+    console.log("ALL EVENTS: ", events);
+    res.render('listings', {events: events});
+  });
+  // console.log("/events");
+  // res.render('listings', { events: events });
 });
 
 router.get('/id/:eventId', function(req, res) {
   console.log("/events/id");
-  console.log("eid: ", )
+  console.log("eid: ", req.params)
   res.render('listing');
+});
+
+router.get('/checkout', function(req, res) {
+  res.render('checkout.ejs');
 });
 
 router.get('/create', function(req, res) {
